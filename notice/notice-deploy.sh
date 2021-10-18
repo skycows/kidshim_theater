7cb135992551688e0f60b69345873b33b548c15f
skycows <skycows3@gmail.com>
2021-10-18 23:24:20 +0900

    local환경 수정

--- notice/notice-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-
-docker build -t kidshim/kakao:v1 .
-docker push kidshim/kakao:v1
-
-kubectl delete deploy kakao
-kubectl create deploy kakao --image=kidshim/kakao:v1
-kubectl expose deploy kakao --type="ClusterIP" --port=8080
